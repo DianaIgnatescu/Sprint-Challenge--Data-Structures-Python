8e122b78544b51e942ac3fec285a0fc751299ef8
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-14 08:23:44 +0100

    Implement get method & check that test passes

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -12,5 +12,29 @@ class RingBuffer:
             self.current = 0
         else:
             self.current += 1
+
+    # The get method returns all of the elements in the buffer in a list in their given order.
+    # It should not return any None values in the list even if they are present in the ring buffer.
     def get(self):
-        pass
+        result = [i for i in self.storage if i is not None]
+        return result
+
+# buffer = RingBuffer(3)
+#
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+#
+# print(buffer.get())
+#
+# buffer.get()   # should return ['a', 'b', 'c']
+#
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+#
+# buffer.get()   # should return ['d', 'b', 'c']
+#
+# buffer.append('e')
+# buffer.append('f')
+#
+# print(buffer.get())   # should return ['d', 'e', 'f']
