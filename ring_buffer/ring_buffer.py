7b9cdc166cba192806d6a52e05365ff82451aeb9
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-14 10:50:31 +0100

    Remove redundancies

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -18,23 +18,3 @@ class RingBuffer:
     def get(self):
         result = [i for i in self.storage if i is not None]
         return result
-
-# buffer = RingBuffer(3)
-#
-# buffer.append('a')
-# buffer.append('b')
-# buffer.append('c')
-#
-# print(buffer.get())
-#
-# buffer.get()   # should return ['a', 'b', 'c']
-#
-# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
-# buffer.append('d')
-#
-# buffer.get()   # should return ['d', 'b', 'c']
-#
-# buffer.append('e')
-# buffer.append('f')
-#
-# print(buffer.get())   # should return ['d', 'e', 'f']
