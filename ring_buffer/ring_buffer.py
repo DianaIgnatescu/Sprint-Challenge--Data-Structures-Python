f6346efdeb82ee2f77747173c566e9edaf1c2173
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-14 08:23:06 +0100

    Implement append method

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,8 +4,13 @@ class RingBuffer:
         self.current = 0
         self.storage = [None]*capacity
 
+    # The append method adds elements to the buffer
     def append(self, item):
-        pass
+        self.storage[self.current] = item
 
+        if self.current == self.capacity - 1:
+            self.current = 0
+        else:
+            self.current += 1
     def get(self):
         pass
